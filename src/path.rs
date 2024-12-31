652f5951af90b4bc9e6771cf57bd52aed26117cd
Wisdom Ogwu <iammadab@gmail.com>
2024-12-31 12:23:54 +0100

    implement path check for edge list

--- src/path.rs
+++ src/path.rs
@@ -19,6 +19,35 @@ pub(crate) fn check_node_path_valid(g: &Graph, path: Vec<usize>) -> bool {
     true
 }
 
+/// path representation => list of edges
+/// path description: after traversing edge i, traverse edge i + 1
+/// validity constraint: each edge must be an actual edge, the destination of edge i should be that
+/// start of edge i + 1
+pub(crate) fn check_edge_path_valid(g: &Graph, path: Vec<(usize, usize)>) -> bool {
+    // empty paths are considered valid
+    if path.is_empty() {
+        return true;
+    }
+
+    for i in 1..path.len() {
+        // validate first edge
+        if i == 1 && !g.is_edge(path[i - 1].0, path[i - 1].1) {
+            return false;
+        }
+
+        if !g.is_edge(path[i].0, path[i].1) {
+            return false;
+        }
+
+        // ensure destination of previous edge is the start of current edge
+        if path[i - 1].1 != path[i].0 {
+            return false;
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{path::check_node_path_valid, tests::undirected_graph};
