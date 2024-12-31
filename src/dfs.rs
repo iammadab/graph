d2ae282de174e712a6c9463aef5aeff1a9baf693
Wisdom Ogwu <iammadab@gmail.com>
2024-12-31 17:19:01 +0100

    implement dfs that records path

--- src/dfs.rs
+++ src/dfs.rs
@@ -41,3 +41,25 @@ pub(crate) fn dfs_recursive_basic<F>(
         }
     }
 }
+
+/// Performs dfs and records path information in prev_node representation
+/// requires a starting node
+pub(crate) fn dfs_path(g: &Graph, start_node: usize) -> Vec<Option<usize>> {
+    let mut prev_node_list = vec![None; g.num_of_nodes()];
+    let update_list = |prev_node, new_node| {
+        prev_node_list[new_node] = Some(prev_node);
+    };
+    dfs_basic(g, start_node, update_list);
+    prev_node_list
+}
+
+/// Performs dfs and records path information in prev_node representation
+/// doesn't require a starting node
+pub(crate) fn dfs_path_all(g: &Graph) -> Vec<Option<usize>> {
+    let mut prev_node_list = vec![None; g.num_of_nodes()];
+    let update_list = |prev_node, new_node| {
+        prev_node_list[new_node] = Some(prev_node);
+    };
+    dfs_basic_all(g, update_list);
+    prev_node_list
+}
