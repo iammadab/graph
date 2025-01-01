3b5a16877897439c229a5b26cfa71ace1e5e805e
Wisdom Ogwu <iammadab@gmail.com>
2025-01-01 12:17:49 +0100

    bfs test passing

--- src/bfs.rs
+++ src/bfs.rs
@@ -6,14 +6,14 @@ pub(crate) fn bfs(g: &Graph, start_node: usize) -> Vec<Option<usize>> {
     let mut prev_node_list = vec![None; g.num_of_nodes()];
     let mut queue = VecDeque::from([start_node]);
 
+    seen[start_node] = true;
+
     while let Some(node_id) = queue.pop_front() {
-        if !seen[node_id] {
-            seen[node_id] = true;
-            for neighbor in g.nodes[node_id].get_neighbors() {
-                if !seen[neighbor] {
-                    prev_node_list[neighbor] = Some(node_id);
-                    queue.push_back(neighbor);
-                }
+        for neighbor in g.nodes[node_id].get_neighbors() {
+            if !seen[neighbor] {
+                seen[neighbor] = true;
+                prev_node_list[neighbor] = Some(node_id);
+                queue.push_back(neighbor);
             }
         }
     }
@@ -23,12 +23,18 @@ pub(crate) fn bfs(g: &Graph, start_node: usize) -> Vec<Option<usize>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::bfs::bfs;
+    use crate::bfs::{self, bfs};
     use crate::tests::ten_node_undirected_graph;
 
     #[test]
     fn test_bfs() {
         let g = ten_node_undirected_graph();
-        dbg!(bfs(&g, 0));
+        assert_eq!(
+            bfs(&g, 0)
+                .into_iter()
+                .map(|v| v.map(|v| v as isize).unwrap_or(-1))
+                .collect::<Vec<_>>(),
+            vec![-1, 0, 1, 2, 2, 0, 5, 0, 5, 8]
+        );
     }
 }
