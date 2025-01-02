f7185e9647cb2ab48cd114d1d7826c4d4574281b
Wisdom Ogwu <iammadab@gmail.com>
2025-01-02 09:16:39 +0100

    basic graph structure

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,9 +1,13 @@
-pub(crate) trait Graph<'a> {
+pub(crate) mod static_graph;
+
+// TODO: add documentation
+pub(crate) trait Graph {
     type NodeType: Node;
 
-    fn get_node(node_id: usize) -> Option<&'a Self::NodeType>;
+    fn get_node(&self, node_id: usize) -> Option<&Self::NodeType>;
+    fn num_of_nodes(&self) -> Option<usize>;
 }
 
 pub(crate) trait Node {
-    fn neighbors(&self) -> Vec<usize>;
+    fn neighbors(&self) -> &[usize];
 }
---
+++ src/graph/static_graph.rs
@@ -0,0 +1,28 @@
+use crate::graph::{Graph, Node};
+
+struct StaticNode {
+    edges: Vec<usize>,
+}
+
+impl Node for StaticNode {
+    fn neighbors(&self) -> &[usize] {
+        &self.edges
+    }
+}
+
+// Graph
+struct StaticGraph {
+    nodes: Vec<StaticNode>,
+}
+
+impl Graph for StaticGraph {
+    type NodeType = StaticNode;
+
+    fn get_node(&self, node_id: usize) -> Option<&Self::NodeType> {
+        self.nodes.get(node_id)
+    }
+
+    fn num_of_nodes(&self) -> Option<usize> {
+        Some(self.nodes.len())
+    }
+}
